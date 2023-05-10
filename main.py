31af6c835670104a072089ae8a74c90e02e6bfe4
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-10 12:47:07 +0200

    solved the task :)

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ new_dict = {row.letter: row.code for index, row in nato_data_frame.iterrows()}
 
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_name = "thomas"
+user_name = input("Enter your name: ")
 name_list = [name.upper() for name in user_name]
 nato_spelling = [new_dict[letter] for letter in name_list if letter in list(new_dict.keys())]
 
