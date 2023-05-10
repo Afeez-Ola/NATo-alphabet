4d136fcc6f2ac7a3174d2c9dfe64663932c90691
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-10 12:39:51 +0200

    solved the task :)

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-# student_dict = {
-#     "student": ["Angela", "James", "Lily"],
-#     "score": [56, 76, 98]
-# }
-#
-# #Looping through dictionaries:
-# for (key, value) in student_dict.items():
-#     #Access key and value
-#     pass
 
 import pandas
 
 nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 # TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 new_dict = {row.letter: row.code for index, row in nato_data_frame.iterrows()}
 
 
