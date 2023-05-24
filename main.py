1e7f04bccfc51c53fb2e722077cbae552e43887b
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-24 08:55:40 +0200

    Refactored by capturing the errors

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-
 import pandas
 
 nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-# TODO 1. Create a dictionary in this format:
 new_dict = {row.letter: row.code for index, row in nato_data_frame.iterrows()}
+def nato_spelling():
+    nato_letters = ""
+    try:
+        word = input("Enter your word: ").upper()
+        nato_letters = [new_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        nato_spelling()
+    print(nato_letters)
+
+nato_spelling()
 
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_name = input("Enter your name: ")
-name_list = [name.upper() for name in user_name]
-nato_spelling = [new_dict[letter] for letter in name_list if letter in list(new_dict.keys())]
 
-print(nato_spelling)
+
+
+
